beaaff1b0858f993e30f8a4aef3c3d729f05c757
xyb <xyb@xyb.name>
2020-11-29 05:48:41 -0800

    `src` in configuration file supports string or array of strings.

--- test/ConfigTest.cs
+++ test/ConfigTest.cs
@@ -27,6 +27,18 @@ namespace CssBuilder.Test
         }
 
         [Fact]
+        public void SingleInputOutputIsFile()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_SingleInputOutputIsFile");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "a.css", null);
+            VerifyFiles(testFolder, "a.less", "out/overall.css", "body {\n  color: red;\n}\n");
+        }
+
+        [Fact]
         public void SingleInputOutputIsFolder()
         {
             var testFolder = PrepareTestFolder("ConfigTest_SingleInputOutputIsFolder");
@@ -35,7 +47,7 @@ namespace CssBuilder.Test
             Assert.Equal(0, exitCode);
 
             VerifyFiles(testFolder, "a.less", "a.css", null);
-            VerifyFiles(testFolder, "a.less", "out/overall.css", "body {\n  color: red;\n}\n");
+            VerifyFiles(testFolder, "a.less", "out/a.css", "body {\n  color: red;\n}\n");
         }
 
         [Fact]
@@ -49,6 +61,20 @@ namespace CssBuilder.Test
             VerifyFiles(testFolder, "a.less", "a.css", "body {\n  color: red;\n}\n");
             VerifyFiles(testFolder, "b/b.less", "b/b.css", "body {\n  color: blue;\n}\n");
             VerifyFiles(testFolder, "b/c/c.less", "b/c/c.css", "body {\n  color: yellow;\n}\n");
+        }
+
+        [Fact]
+        public void GlobInputOutputIsFile()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_GlobInputOutputIsFile");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "out/a.css", null);
+            VerifyFiles(testFolder, "b/b.less", "out/b.css", null);
+            VerifyFiles(testFolder, "b/c/c.less", "out/c.css", null);
+            VerifyFiles(testFolder, null, "out/overall.css", "body {\n  color: red;\n}\nbody {\n  color: blue;\n}\nbody {\n  color: yellow;\n}\n");
         }
 
         [Fact]
@@ -73,6 +99,86 @@ namespace CssBuilder.Test
             Assert.Equal(0, exitCode);
 
             VerifyFiles(testFolder, "a.less", "out/overall.css", "body {\n  color: red;\n}\n");
+            VerifyFiles(testFolder, "b/b.less", "out/b.css", "body {\n  color: blue;\n}\n");
+            VerifyFiles(testFolder, "b/c/c.less", "out/c.css", "body {\n  color: yellow;\n}\n");
+        }
+
+        [Fact]
+        public void MultipleInputNoOutputSetting()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_MultipleInputNoOutputSetting");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "a.css", "body {\n  color: red;\n}\n");
+            VerifyFiles(testFolder, "b.less", "b.css", "body {\n  color: blue;\n}\n");
+            VerifyFiles(testFolder, "c.sass", "c.css", null);
+        }
+
+        [Fact]
+        public void MultipleInputOutputIsFile()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_MultipleInputOutputIsFile");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "a.css", null);
+            VerifyFiles(testFolder, "b.less", "b.css", null);
+            VerifyFiles(testFolder, null, "out/overall.css", "body {\n  color: red;\n}\nbody {\n  color: blue;\n}\n");
+        }
+
+
+        [Fact]
+        public void MultipleInputOutputIsFolder()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_MultipleInputOutputIsFolder");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "out/a.css", "body {\n  color: red;\n}\n");
+            VerifyFiles(testFolder, "b/b.less", "out/b.css", null);
+            VerifyFiles(testFolder, "b/c/c.less", "out/c.css", "body {\n  color: yellow;\n}\n");
+        }
+
+        [Fact]
+        public void SingleAndGlobInputNoOutputSetting()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_SingleAndGlobInputNoOutputSetting");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "a.css", "body {\n  color: red;\n}\n");
+            VerifyFiles(testFolder, "b/b.less", "b/b.css", "body {\n  color: blue;\n}\n");
+            VerifyFiles(testFolder, "b/c/c.less", "b/c/c.css", "body {\n  color: yellow;\n}\n");
+        }
+
+        [Fact]
+        public void SingleAndGlobInputOutputIsFile()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_SingleAndGlobInputOutputIsFile");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "a.css", null);
+            VerifyFiles(testFolder, "b/b.less", "b/b.css", null);
+            VerifyFiles(testFolder, "b/c/c.less", "b/c/c.css", null);
+            VerifyFiles(testFolder, null, "overall.css", "body {\n  color: red;\n}\nbody {\n  color: blue;\n}\nbody {\n  color: yellow;\n}\n");
+        }
+
+        [Fact]
+        public void SingleAndGlobInputOutputIsFolder()
+        {
+            var testFolder = PrepareTestFolder("ConfigTest_SingleAndGlobInputOutputIsFolder");
+
+            var exitCode = CssBuilder.Program.Main(new string[] { testFolder });
+            Assert.Equal(0, exitCode);
+
+            VerifyFiles(testFolder, "a.less", "out/a.css", "body {\n  color: red;\n}\n");
             VerifyFiles(testFolder, "b/b.less", "out/b.css", "body {\n  color: blue;\n}\n");
             VerifyFiles(testFolder, "b/c/c.less", "out/c.css", "body {\n  color: yellow;\n}\n");
         }
